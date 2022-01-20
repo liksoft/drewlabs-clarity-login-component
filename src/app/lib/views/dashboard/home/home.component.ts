2fd373db7565d958f67d068e7f972bd681a86c4d
adevou <ceo@coucoumail.com>
2022-01-20 15:43:11 +0000

    Dashboard OK

--- src/app/lib/views/dashboard/home/home.component.ts
+++ src/app/lib/views/dashboard/home/home.component.ts
@@ -10,5 +10,43 @@ import { RoutesMap } from "src/app/lib/core/routes";
 export class AdminDashboardHomeComponent {
   public navbarRoutesMap: RoutesMap[];
 
+  public transactions = [
+    {
+      "id" : 1,
+      "date" : "2022-01-10 12:45:44",
+      "partenaire" : "53021 BTCI",
+      "transaction" : 12,
+      "fichier" : "http://localhost:22000/dashboard/home",
+    },
+    {
+      "id" : 2,
+      "date" : "2022-01-11 12:45:44",
+      "partenaire" : "53021 BTCI",
+      "transaction" : 45,
+      "fichier" : "http://localhost:22000/dashboard/home",
+    },
+    {
+      "id" : 3,
+      "date" : "2022-01-12 12:45:44",
+      "partenaire" : "53021 BTCI",
+      "transaction" : 56,
+      "fichier" : "http://localhost:22000/dashboard/home",
+    },
+    {
+      "id" : 4,
+      "date" : "2022-01-13 12:45:44",
+      "partenaire" : "53021 BTCI",
+      "transaction" : 85,
+      "fichier" : "http://localhost:22000/dashboard/home",
+    },
+    {
+      "id" : 5,
+      "date" : "2022-01-14 12:45:44",
+      "partenaire" : "53021 BTCI",
+      "transaction" : 0,
+      "fichier" : "http://localhost:22000/dashboard/home",
+    },
+
+  ];
   constructor(private auth: AuthService) {}
 }
